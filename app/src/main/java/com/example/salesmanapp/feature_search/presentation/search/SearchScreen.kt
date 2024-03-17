6822d7e0dd53751508a99fc21e4e73c43c104b7d
Patryk Rutkowski <patrykk.rutkowski@gmail.com>
2024-03-17 14:19:32 +0100

    Added loading state and debunce on search

--- app/src/main/java/com/example/salesmanapp/feature_search/presentation/search/SearchScreen.kt
+++ app/src/main/java/com/example/salesmanapp/feature_search/presentation/search/SearchScreen.kt
@@ -1,9 +1,13 @@
 package com.example.salesmanapp.feature_search.presentation.search
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -24,15 +28,27 @@ fun SearchScreen() {
             )
         }
 
-        items(
-            count = viewModel.salesmanList.size,
-            key = { viewModel.salesmanList[it].name },
-        ) {
-            SalesmanRow(
-                name = viewModel.salesmanList[it].name,
-                postCodes = viewModel.salesmanList[it].areas,
-                modifier = Modifier.padding(16.dp)
-            )
+        if (viewModel.isLoading.value) {
+            item {
+                Box(
+                    modifier = Modifier.fillMaxWidth(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator()
+                }
+            }
+        }
+        else {
+            items(
+                count = viewModel.salesmanList.size,
+                key = { viewModel.salesmanList[it].name },
+            ) {
+                SalesmanRow(
+                    name = viewModel.salesmanList[it].name,
+                    postCodes = viewModel.salesmanList[it].areas,
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
         }
     }
 }
--- app/src/main/java/com/example/salesmanapp/feature_search/presentation/search/SearchScreenViewModel.kt
+++ app/src/main/java/com/example/salesmanapp/feature_search/presentation/search/SearchScreenViewModel.kt
@@ -9,10 +9,18 @@ import androidx.lifecycle.viewModelScope
 import com.example.salesmanapp.feature_search.domain.model.Salesman
 import com.example.salesmanapp.feature_search.domain.use_case.GetSalesmanListByArea
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.FlowPreview
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.debounce
 import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onCompletion
 import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+private val SEARCH_DELAY = 1000L
+
+@OptIn(FlowPreview::class)
 @HiltViewModel
 class SearchScreenViewModel
 @Inject
@@ -20,17 +28,35 @@ constructor(
     private val getSalesmanListByArea: GetSalesmanListByArea
 ) : ViewModel() {
 
+    private val searchFlow: MutableSharedFlow<String> = MutableSharedFlow()
+
     private val _searchValue = mutableStateOf("")
     val searchValue: State<String> = _searchValue
+
+    private val _isLoading = mutableStateOf(false)
+    val isLoading: State<Boolean> = _isLoading
 
     private val _salesmanList = mutableStateListOf<Salesman>()
     val salesmanList: SnapshotStateList<Salesman> = _salesmanList
 
+    init {
+        searchFlow
+            .debounce(SEARCH_DELAY)
+            .onEach { postCode ->
+                getSalesman(postCode)
+            }
+            .launchIn(viewModelScope)
+    }
+
     fun dispatchAction(action: SearchScreenAction) {
         when (action) {
             is SearchScreenAction.SearchValueChanged -> {
+                _isLoading.value = true
                 _searchValue.value = action.searchValue
-                getSalesman(action.searchValue)
+                viewModelScope.launch {
+                    searchFlow
+                        .emit(action.searchValue)
+                }
             }
         }
     }
@@ -41,6 +67,7 @@ constructor(
                 _salesmanList.clear()
                 _salesmanList.addAll(salesmanList)
             }
+            .onCompletion { _isLoading.value = false }
             .launchIn(viewModelScope)
 
 }
