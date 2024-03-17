197babe65b54876bc5cbdd69e996331f49e9aa58
Patryk Rutkowski <patrykk.rutkowski@gmail.com>
2024-03-17 10:28:01 +0100

    Added empty search screen

--- app/src/main/java/com/example/salesmanapp/feature_search/MainActivity.kt
+++ app/src/main/java/com/example/salesmanapp/feature_search/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.salesmanapp
+package com.example.salesmanapp.feature_search
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -6,10 +6,8 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import com.example.salesmanapp.feature_search.presentation.search.SearchScreen
 import com.example.salesmanapp.ui.theme.SalesmanAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,25 +20,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    SearchScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    SalesmanAppTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/com/example/salesmanapp/feature_search/presentation/search/SearchScreenViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.salesmanapp.feature_search.presentation.search
+
+import androidx.lifecycle.ViewModel
+
+class SearchScreenViewModel: ViewModel() {
+
+}
